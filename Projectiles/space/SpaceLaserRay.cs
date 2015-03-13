a394412f761710837cf582df2e1f27b724108276
LolHens <pierrekisters@gmail.com>
2015-03-14 00:47:01 +0100

    added SpaceLaserRay json + cs (WIP)

---
+++ Projectiles/space/SpaceLaserRay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolHens.Projectiles
+{
+    public class SpaceLaserRay : LolHensProjectile
+    {
+        public int type;
+        public float splitsLeft = 16;
+
+        public override void Init()
+        {
+            projectile.hurtsTiles = false;
+        }
+    }
+}
