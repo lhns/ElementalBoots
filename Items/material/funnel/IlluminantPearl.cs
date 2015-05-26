3ad41507248badbeff50db5c90855696bebe09bc
LolHens <pierrekisters@gmail.com>
2015-05-26 12:31:20 +0200

    -

---
+++ Items/material/funnel/IlluminantPearl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TAPI;
+
+namespace LolHens.Items
+{
+    public class IlluminantPearl: LolHensItem
+    {
+        public override void InitType(Type type)
+        {
+            const float dropChance = 0.0167f;
+
+            NPCDef.byName["Vanilla:Illuminant Bat"].AddDrop(item, dropChance);
+            NPCDef.byName["Vanilla:Illuminant Slime"].AddDrop(item, dropChance);
+            NPCDef.byName["Vanilla:Chaos Elemental"].AddDrop(item, dropChance);
+            NPCDef.byName["Vanilla:Enchanted Sword"].AddDrop(item, dropChance);
+        }
+    }
+}
