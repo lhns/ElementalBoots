a4b1a2e2dc1a98ef5e9f66538b63854ac2a75dd9
Pierre Kisters <pierrekisters@gmail.com>
2018-04-08 13:10:40 +0200

    bandofstamina and bandofanarchy

---
+++ Items/Accessories/Bands/BandOfAnarchy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace ElementalBoots.Items.Accessories.Bands
+{
+    class BandOfAnarchy : CompoundAccessory
+    {
+        public override IList<Item> GetCompoundAccessories()
+        {
+            return new List<Item>
+            {
+                ElementalBootsMod.instance.GetItemType("BandOfStamina"),
+                ElementalBootsMod.instance.GetItemType(ItemID.AvengerEmblem),
+                ElementalBootsMod.instance.GetItemType(ItemID.BandofStarpower),
+                ElementalBootsMod.instance.GetItemType(ItemID.BandofRegeneration)
+            };
+        }
+    }
+}
