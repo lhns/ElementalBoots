fae631f08bbc0db8400ded7cd3e616f14d08c08c
Pierre Kisters <pierrekisters@gmail.com>
2018-04-13 23:43:36 +0200

    IceStorm

---
+++ Items/Weapons/IceStorm/IceStorm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+
+namespace ElementalBoots.Items.Weapons.IceStorm
+{
+    class IceStorm: Weapon
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            item.value = Value.SILVER * 10;
+            item.rare = 4;
+
+            item.useStyle = 4;
+            item.useAnimation = 10;
+            item.useTime = 10;
+            item.UseSound = new LegacySoundStyle(2, 9);
+
+            item.damage = 35;
+            item.knockBack = 10;
+            item.autoReuse = true;
+            item.noMelee = true;
+            item.magic = true;
+            item.shoot = mod.ProjectileType("IceCrystal");
+            item.shootSpeed = 2;
+            item.mana = 2;
+        }
+    }
+}
